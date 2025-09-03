65e12b4af2ff34cc484bf669c0f7ab0da09fc52d
saquib27 <saquibshamshi27@gmail.com>
2025-09-03 21:19:25 +0530

    bitwise

--- bitwise.cpp
+++ bitwise.cpp
@@ -1,34 +1,26 @@
 #include <iostream>
 using namespace std;
 
-int main() {
-    int a,b;
-    cout<<"enter first number:  ";
-    cin>>a;
-    cout<<"enter second number:  ";
-    cin>>b;
+int main()
+{
+    int a, b;
+    cout << "Enter first number: ";
+    cin >> a;
+    cout << "Enter second number: ";
+    cin >> b;
 
-    
     int bitwise_and = a & b;
-
-
     int bitwise_or = a | b;
-
-
     int bitwise_xor = a ^ b;
-
     int bitwise_not = ~b;
 
-    
-    int left_shift = 5 << 2;
-
-    
-    int right_shift = 16 >> 2;
+    int left_shift = a << b;
+    int right_shift = a >> b;
 
     cout << "AND: " << bitwise_and << endl;
     cout << "OR: " << bitwise_or << endl;
     cout << "XOR: " << bitwise_xor << endl;
-    cout << "NOT b: " << bitwise_not << endl;
+    cout << "NOT: " << bitwise_not << endl;
     cout << "Left Shift: " << left_shift << endl;
     cout << "Right Shift: " << right_shift << endl;
 
