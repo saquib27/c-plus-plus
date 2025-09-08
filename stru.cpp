67bbc0ec54b64479390c8287671a91d0bb1f51d5
saquib27 <saquibshamshi27@gmail.com>
2025-09-08 23:00:19 +0530

    structure

---
+++ stru.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+struct Point {
+private:
+    int x, y;
+public:
+
+    Point(int a, int b) {
+        x = a;
+        y = b;
+    }
+    
+    void show() {
+        cout << x << " " << y << endl;
+    }
+    
+    // Destructor
+    ~Point() {
+        cout << "destroyed point variable" << endl;
+    }
+};
+
+int main() {
+    Point s1 (1,1);
+    Point s2(99, 1001);
+
+    s1.show();
+    s2.show();
+    return 0;
+}
