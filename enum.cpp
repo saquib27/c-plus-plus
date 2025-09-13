b85b09b14de9063d1de013a144a19e9c0f4281d7
saquib27 <saquibshamshi27@gmail.com>
2025-09-13 23:27:59 +0530

    enum

--- enum.cpp
+++ enum.cpp
@@ -1,17 +1,32 @@
 #include <iostream>
+#include <limits.h>
 using namespace std;
 
 enum fruit {
-    APPLE, BANANA = 5, ORANGE
+    APPLE,      
+    BANANA = 5,
+    ORANGE     
 };
 
+string getFruitName(fruit f) {
+    switch (f) {
+        case APPLE: return "APPLE";
+        case BANANA: return "BANANA";
+        case ORANGE: return "ORANGE";
+        default: return "UNKNOWN";
+    }
+}
+
 int main() {
-
-
     fruit f = BANANA;
-    cout << f << endl;
-    
+    cout << "Enum value of BANANA: " << f << endl;
+    cout << "Fruit name: " << getFruitName(f) << endl;
+
     f = ORANGE;
-    cout << f;
+    cout << "Enum value of ORANGE: " << f << endl;
+    cout << "Fruit name: " << getFruitName(f) << endl;
+
+    cout << "Direct integer 5: " << 5 << endl;
+
     return 0;
 }
